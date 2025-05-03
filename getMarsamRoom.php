e61e0ae60bede006945ed0077f8ebf9b3a4ad3b7
Primo <150794304+pprim0@users.noreply.github.com>
2025-05-03 01:52:16 +0100

    Add files via upload

---
+++ getMarsamRoom.php
@@ -0,0 +1,28 @@
+<?php
+$db = "railway";
+$dbhost = "trolley.proxy.rlwy.net";
+$username = "root";$_POST["username"];
+$password = "UeFiSShvSBqwfJsKcOvakNnSjvteBkZr";$_POST["password"];
+
+$conn = mysqli_connect($dbhost, $username, $password, $db, 22777);// Create connection
+
+if (!$conn) {// Check connection
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM `ocupaçãolabirinto` WHERE `IDJogo` = (SELECT MAX(`IDJogo`) FROM `ocupaçãolabirinto`) ORDER BY `Sala`;";
+
+$result = mysqli_query($conn, $sql);// Execute the query
+$response = array();
+if (mysqli_num_rows($result) > 0) {// Check if any row is returned
+     while ($row = mysqli_fetch_assoc($result)) {//Iterates trough the result and puts entries in response
+        array_push($response, $row);
+    }
+} else {
+    $response = array();// If no rows are returned, initialize an empty response
+}
+
+mysqli_close($conn);// Close the connection
+
+echo json_encode($response);// Convert the response array to JSON format
+?>
