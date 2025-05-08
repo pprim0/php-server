9846efba66ad46a9d565147a95a42c9206d2fbba
Primo <150794304+pprim0@users.noreply.github.com>
2025-05-08 06:31:26 +0100

    Update obterPontuacao.php

--- obterPontuacao.php
+++ obterPontuacao.php
@@ -1,11 +1,31 @@
--<?php
-    $db = "railway";
-	$dbhost = "trolley.proxy.rlwy.net";
-	$username = $_POST["username"];
-	$password = $_POST["password"];
-	$conn = mysqli_connect($dbhost, $username, $password, $db, 22777);	
-	$sql = "CALL getPoints()";
-	$result = mysqli_query($conn, $sql);
-	mysqli_close ($conn);
-	echo json_encode($result);
+<?php
+$db = "railway";
+$dbhost = "trolley.proxy.rlwy.net";
+$dbport = 22777; 
+$username = $_POST["username"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+
+$conn = mysqli_connect($dbhost, $username, $password, $db, $dbport);
+
+$response = array();
+$response["success"] = false;
+
+$query = "SELECT Score FROM Jogo 
+          JOIN Utilizador ON Jogo.Email = Utilizador.Email 
+          WHERE Utilizador.Email = '$email' AND Jogo.IsRunning = 1 
+          ORDER BY IDJogo DESC LIMIT 1";
+
+$result = mysqli_query($conn, $query);
+
+if ($result && mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $response["success"] = true;
+    $response["score"] = $row["Score"];
+} else {
+    $response["message"] = "Nenhum jogo ativo encontrado.";
+}
+
+header('Content-Type: application/json');
+echo json_encode($response);
 ?>
