b5bafa4b712b5fec41fffa535a6f42c5393de648
Primo <150794304+pprim0@users.noreply.github.com>
2025-05-15 18:18:31 +0100

    Update iniciarJogo.php

--- iniciarJogo.php
+++ iniciarJogo.php
@@ -8,6 +8,12 @@ $password = $_POST["password"];
 $email = $_POST["email"];
 $descricao = $_POST["descricao"];
 
+header('Access-Control-Allow-Origin: *'); // Permitir chamadas de qualquer origem
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Content-Type: application/json');
+
+
 $conn = mysqli_connect($dbhost, $username, $password, $db, $port);
 
 if (!$conn) {
