ae6a3225f19df69bc29dd80f6390008a69228ea1
Primo <150794304+pprim0@users.noreply.github.com>
2025-05-15 19:18:48 +0100

    Update obterPontuacao.php

--- obterPontuacao.php
+++ obterPontuacao.php
@@ -8,7 +8,7 @@ $username = $_POST["username"];
 $password = $_POST["password"];
 $email = $_POST["email"];
 
-eader('Access-Control-Allow-Origin: *'); // Permitir chamadas de qualquer origem
+header('Access-Control-Allow-Origin: *'); // Permitir chamadas de qualquer origem
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json');
